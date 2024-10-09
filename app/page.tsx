8ea2750538b1a898fdb033593f6737701e3f8f80
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-10-09 22:15:54 +0330

    CHORE: add MUI free template and also add sign-in, sign-up, dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,13 @@
 
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import styles from "./page.module.css";
-import MarketingPage from "@/components/MarketingPage";
-import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
+import MarketingPage from "@/components/marketing-page/MarketingPage";
 
-const theme = createTheme();
 export default function Home() {
   return (
     <AppRouterCacheProvider>
       <main className={styles.main}>
-        <ThemeProvider theme={theme}>
-          <MarketingPage />
-        </ThemeProvider>
+        <MarketingPage />
       </main>
     </AppRouterCacheProvider>
   );
