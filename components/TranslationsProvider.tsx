336bcde49656fe4846159a17efe4d25a2ed7728a
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-10-18 17:56:18 +0330

    feat(project): add localization

---
+++ components/TranslationsProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { I18nextProvider } from "react-i18next";
+import initTranslations from "@/app/i18n";
+import { createInstance, Resource } from "i18next";
+import { ReactProps } from "@/types/defaultProps";
+
+interface Props extends ReactProps {
+  resources: Resource;
+}
+
+export default function TranslationsProvider({ children, locale, resources }: Props) {
+  const i18n = createInstance();
+
+  initTranslations(locale, i18n, resources);
+
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
+}
---
+++ types/defaultProps.d.ts
@@ -0,0 +1,6 @@
+export interface ReactProps {
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+  key?: string | number;
+  locale?: "fa" | "en";
+}
