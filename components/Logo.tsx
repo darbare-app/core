073986bd889460d7136fc29d091edf7f5c0a63c7
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-10-24 18:27:55 +0330

    feat(logo): add translation

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import logo from "/public/darbare.jpg";
 import { Box, Typography } from "@mui/material";
+import { useTranslation } from 'react-i18next';
 
 export default function Logo() {
+  const {t} = useTranslation()  
   return (
     <Box sx={{ display: "flex", alignItems: "center", gap: ".5rem", mr: 1 }}>
       <Image alt="Darbare logo" src={logo} height={30} style={{ borderRadius: "50%" }} />
-      <Typography sx={{ color: "hsl(210, 100%, 65%)" }}>ABOUT</Typography>
+      <Typography sx={{ color: "hsl(210, 100%, 65%)" }}>{t("darbare")}</Typography>
     </Box>
   );
 }
