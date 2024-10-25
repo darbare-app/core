ce45b6145cd508ee4d659ea0cfa197b6e5b99d1b
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-10-25 17:58:09 +0330

    chore(reactFiles): remove all react imports
      with this script npx react-codemod
      update-react-imports

--- app/[local]/blog/page.tsx
+++ app/[local]/blog/page.tsx
@@ -1,5 +1,4 @@
 import Blog from "@/components/blog/Blog";
-import React from "react";
 
 export default function page() {
   return <Blog />;
--- app/[local]/sign-in/page.tsx
+++ app/[local]/sign-in/page.tsx
@@ -1,5 +1,4 @@
 import SignIn from "@/components/sign-in/SignIn";
-import React from "react";
 
 export default function page() {
   return <SignIn />;
