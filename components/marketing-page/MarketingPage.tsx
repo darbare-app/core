ab1fb3d4ec87fe62de340abf22487acbd3ab8878
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-11-08 15:20:20 +0330

    chore(marketingPage): remove unwanted components

--- components/marketing-page/MarketingPage.tsx
+++ components/marketing-page/MarketingPage.tsx
@@ -1,14 +1,8 @@
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import AppAppBar from "../AppAppBar";
 import Hero from "./components/Hero";
-import LogoCollection from "./components/LogoCollection";
-import Highlights from "./components/Highlights";
-import Pricing from "./components/Pricing";
-import Features from "./components/Features";
-import Testimonials from "./components/Testimonials";
-import FAQ from "./components/FAQ";
 import Footer from "../Footer";
+import { Divider } from "@mui/material";
 
 export default function MarketingPage() {
   return (
@@ -16,20 +10,8 @@ export default function MarketingPage() {
       <CssBaseline enableColorScheme />
       <AppAppBar />
       <Hero />
-      <div>
-        <LogoCollection />
-        <Features />
-        <Divider />
-        <Testimonials />
-        <Divider />
-        <Highlights />
-        <Divider />
-        <Pricing />
-        <Divider />
-        <FAQ />
-        <Divider />
-        <Footer />
-      </div>
+      <Divider />
+      <Footer />
     </>
   );
 }
