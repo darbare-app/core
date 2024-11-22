bfaff8521d19bed2f783805b5e1cecce500776aa
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-11-22 14:50:37 +0330

    refactor(Logo): remove underLine and add IconButton for hover effect

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,24 +1,26 @@
 import Image from "next/image";
 import logo from "/public/darbare.jpg";
-import { Typography } from "@mui/material";
-import { useTranslation } from "react-i18next";
+import { IconButton } from "@mui/material";
 import MuiNextLink from "./CLink";
 
 export default function Logo() {
-  const { t } = useTranslation();
   return (
-    <MuiNextLink
-      href="/"
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        gap: ".5rem",
-        mr: 1,
-        textDecoration: "none !important",
-      }}
+    <IconButton
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center", borderRadius: "50%" }}
     >
-      <Image alt="Darbare logo" src={logo} height={30} style={{ borderRadius: "50%" }} />
-      <Typography sx={{ color: "hsl(210, 100%, 65%)" }}>{t("Darbare")}</Typography>
-    </MuiNextLink>
+      <MuiNextLink
+        href="/"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          "&::before": {
+            height: 0,
+          },
+        }}
+      >
+        <Image alt="Darbare logo" src={logo} height={30} style={{ borderRadius: "50%" }} />
+      </MuiNextLink>
+    </IconButton>
   );
 }
