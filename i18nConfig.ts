a00b9e98bd02c28ce2b427e713417ffa08a5b734
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-11-29 19:44:37 +0330

    fix(i18nConfig): detect correct default local

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -3,5 +3,8 @@ import { Config } from "next-i18n-router/dist/types";
 const i18nConfig: Config = {
   locales: ["fa", "en"],
   defaultLocale: "fa",
+  localeDetector(request: any, config: any) {
+    return request.cookies.NEXT_LOCALE || config.defaultLocale;
+  },
 };
 export default i18nConfig;
