91547e478bf1b37cb41cbaaf0b9bf759e2b92b42
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-12-02 17:06:03 +0330

    feat(theme): implement localstorage theme management

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -1,4 +1,5 @@
 import viewAddresses from "@/utils/viewAddresses.json";
+import { PaletteMode } from "@mui/material";
 
 export function deepCopy(obj: object) {
   return JSON.parse(JSON.stringify(obj));
@@ -6,4 +7,12 @@ export function deepCopy(obj: object) {
 
 export function getLink(viewName: string) {
   return `/${(viewAddresses as any)[viewName]}/`;
+}
+
+export function getLocalSTheme(): PaletteMode {
+  return (localStorage.getItem("themeMode") || "light") as PaletteMode;
+}
+
+export function setLocalThemeMode(mode: PaletteMode) {
+  localStorage.setItem("themeMode", mode);
 }
