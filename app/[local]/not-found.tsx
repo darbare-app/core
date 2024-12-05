e9a50168d01dee043942d01ee346df8b3e4149e6
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2024-12-05 21:41:44 +0330

    refactor(not-found): define variables first

--- app/[local]/not-found.tsx
+++ app/[local]/not-found.tsx
@@ -1,27 +1,5 @@
 import { Fragment } from "react";
 export const runtime = "edge";
-export default function NotFound() {
-  return (
-    <Fragment>
-      <title>404: This page could not be found.</title>
-      <div style={styles.error}>
-        <div>
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
-            }}
-          />
-          <h1 className="next-error-h1" style={styles.h1}>
-            404
-          </h1>
-          <div style={styles.desc}>
-            <h2 style={styles.h2}>This page could not be found.</h2>
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
-}
 
 const styles = {
   error: {
@@ -56,3 +34,26 @@ const styles = {
     margin: 0,
   },
 } as const;
+
+export default function NotFound() {
+  return (
+    <Fragment>
+      <title>404: This page could not be found.</title>
+      <div style={styles.error}>
+        <div>
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
+            }}
+          />
+          <h1 className="next-error-h1" style={styles.h1}>
+            404
+          </h1>
+          <div style={styles.desc}>
+            <h2 style={styles.h2}>This page could not be found.</h2>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
